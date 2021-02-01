9e97971cdf79cd719ba27b666d3160be3ab51d3e
Christopher Belanger <christopher.a.belanger@gmail.com>
2021-02-01 13:45:50 -0500

    minor code cleanup

--- app.R
+++ app.R
@@ -1,11 +1,8 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# COVID VACCINE PREDICTOR
+# Christopher Belanger, 2021-02-01, christopher.a.belanger@gmail.com
+# A demo project to learn more about building Shiny dashboards?
+# A darkly humourous data-driven dive into Canada's COVID-19 vaccination delivery?
+
 
 library(shiny)
 library(shinydashboard)
@@ -85,17 +82,8 @@ all_stats <- read_csv("data/all.csv") %>%
 provinces <- read_csv("data/province_info.csv")
 last_update <- read_csv("data/last_update.csv") %>% pull(1)
 
-# base_url <- "https://api.covid19tracker.ca/reports?stat=vaccinations"
-# url <- base_url
-# 
-# resp <- httr::GET(url)
-# 
-# if (httr::http_type(resp) != "application/json") {
-#     stop("API did not return json.", .call = FALSE)
-# }
 
-# can we make a tibble with the supplementary information?
-#supp_data 
+# make a tibble with the supplementary information
 
 supp_stats <- tibble(    code = all_stats %>%
                              group_by(province) %>%
@@ -136,90 +124,6 @@ all_stats <- all_stats %>%
 
 # define lubridate date-time-stamp
 sf <- stamp("Sunday, January 17, 1999, at 3:34PM")
-# 
-# all_stats %>%
-#     group_by(province) %>%
-#     summarise(date_first_vaccine = all_stats %>%
-#                   group_by(province) %>%
-#                   filter(total_vaccinations > 0) %>%
-#                   arrange(date) %>%
-#                   slice_head(n=1) %>%
-#                   pull(date),
-#               
-#               date_last_vaccine = all_stats %>%
-#                   arrange(date) %>%
-#                   slice_tail(n=1) %>%
-#                   pull(date),
-#               
-#               vaccines_so_far = all_stats %>%
-#                   pull(total_vaccinations) %>%
-#                   max(na.rm = TRUE)
-#     )
-# 
-# jurisdiction_human <- "Canada"
-# 
-# vaccinations <- all_stats %>%
-#     filter(province == "ALL")
-# 
-# date_first_vaccine <- vaccinations %>%
-#     filter(total_vaccinations > 0) %>%
-#     arrange(date) %>%
-#     slice_head(n=1) %>%
-#     pull(date)
-# 
-# date_last_vaccine <- vaccinations %>%
-#     arrange(date) %>%
-#     slice_tail(n=1) %>%
-#     pull(date)
-# 
-# vaccines_so_far <- vaccinations %>%
-#     pull(total_vaccinations) %>%
-#     max()
-# 
-# avg_vaccines_per_minute <- vaccines_so_far / as.numeric(difftime(date_last_vaccine, date_first_vaccine, unit = "mins"))
-# 
-# pop <- 37900000
-# 
-# vaccines_reqd <- pop * 2
-# 
-# mins_to_full_vaccination <- floor(vaccines_reqd / avg_vaccines_per_minute)
-# 
-# date_of_full_vaccination <- Sys.Date() + lubridate::minutes(mins_to_full_vaccination)
-# 
-# predicted_line <- tribble(~date, ~total_vaccinations,
-#                           date_first_vaccine, 0,
-#                           date_of_full_vaccination, vaccines_reqd) %>%
-#     mutate(date = as_date(date))
-# 
-# 
-# 
-# date_string <- sf(date_of_full_vaccination)
-# 
-# 
-# vac_plot <- vaccinations %>%
-#     filter(total_vaccinations > 0) %>%
-#     ggplot(aes(x = date, y = total_vaccinations)) +
-#     geom_line(aes(colour = "Vaccines Administered",
-#                   linetype = "Vaccines Administered")) +
-#     geom_point(aes(colour = "Vaccines Administered",
-#                    linetype = "Vaccines Administered")) +
-#     geom_hline(aes(yintercept = vaccines_reqd, 
-#                    colour = "Total Vaccines Required",
-#                    linetype = "Total Vaccines Required")) +
-#     geom_line(data = predicted_line,
-#               aes(linetype = "Predicted Vaccinations",
-#                   colour = "Predicted Vaccinations")) +
-#     scale_y_continuous(label = scales::comma_format()) +
-#     theme_minimal() +
-#     labs(x = "Date",
-#          y = "Vaccines Administered",
-#          title = paste0(jurisdiction_human," will be fully vaccinated on ", date_string, "!")) +
-#     scale_linetype_manual("Vaccines Administered", values = c("dotted", "twodash", "solid")) +
-#     scale_colour_brewer(palette = "Dark2" )
-# 
-# vac_plot %>%
-#     plotly::ggplotly(dynamicTicks = TRUE)
-
 
 prov_options <- c(provinces$code)
 names(prov_options) <- provinces$name
@@ -246,8 +150,8 @@ tz_abbrs <- c(
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
     
-    
     dashboardHeader(title="COVID Countdown!"),
+    
     dashboardSidebar(
         
         
@@ -349,55 +253,16 @@ server <- function(input, output) {
     })
     
     output$vacs_conclusion_text <- renderText({
-        paste0("If we started when you loaded this page, this will take until ", sf(filteredStats()$date_of_full_vaccination %>% with_tz(tzone = input$tz_select)), " ", tz_abbrs[[input$tz_select]], ".")
+        paste0("If we started when you loaded this page, this would take until ", sf(filteredStats()$date_of_full_vaccination %>% with_tz(tzone = input$tz_select)), " ", tz_abbrs[[input$tz_select]], ".")
     })
     
     output$vaccine_plot <- renderPlotly({
         
-        
         vaccinations <- filteredData()
         statistics <- filteredStats()
-        print(vaccinations)
-        print(statistics)
-        # 
-        # date_first_vaccine <- vaccinations %>%
-        #     filter(total_vaccinations > 0) %>%
-        #     arrange(date) %>%
-        #     slice_head(n=1) %>%
-        #     pull(date)
-        # 
-        # message(paste0("date_first_vacc ", date_first_vaccine))
-        # 
-        # date_last_vaccine <- vaccinations %>%
-        #     arrange(date) %>%
-        #     slice_tail(n=1) %>%
-        #     pull(date)
-        # message(paste0("date last vacc ", date_last_vaccine))
-        # 
-        # vaccines_so_far <- max(vaccinations$total_vaccinations, na.rm = TRUE)
-        # message(paste0("vaccines so far ",vaccines_so_far))
-        # 
-        # 
-        # avg_vaccines_per_minute <- vaccines_so_far / as.numeric(difftime(date_last_vaccine, date_first_vaccine, unit = "mins"))
-        # 
-        # pop <- 37000000
-        # 
-        # prov_code <- filteredData() %>% 
-        #             pull(province) %>%
-        #             head(1)
-        # 
-        # #provinces[code == filteredData()$province[[1]]]$population
-        # pop <- provinces[provinces$code == prov_code,]$population
-        # message(paste("Pop: ", pop))
-        # 
-        # vaccines_reqd <- pop * 2
-        # 
-        # mins_to_full_vaccination <- floor(vaccines_reqd / avg_vaccines_per_minute)
-        # message(paste0("mins to full: ", mins_to_full_vaccination))
-        # 
-        # date_of_full_vaccination <- Sys.Date() + lubridate::minutes(statistics()$mins_to_full_vaccination)
-        # message(paste0("date of full ",statistics()$date_of_full_vaccination))
-        
+        #print(vaccinations)
+        #print(statistics)
+ 
         predicted_line <- tribble(~date, ~total_vaccinations,
                                   statistics$date_first_vaccine, 0,
                                   statistics$date_of_full_vaccination, statistics$vaccines_reqd) %>%
@@ -409,11 +274,7 @@ server <- function(input, output) {
             filter(total_vaccinations > 0) %>%
             ggplot(aes(x = date, y = total_vaccinations)) +
             geom_line(aes(colour = "Administered",
-                          linetype = "Administered",
-                          #    text = paste0("Jurisdiction: ", name,
-                          #                  "\nDate: ", date,
-                          #                  "\nVaccines Administered: ", total_vaccinations)
-            )) +
+                          linetype = "Administered")) +
             geom_point(aes(colour = "Administered",
                            linetype = "Administered",
                            text = paste0("Jurisdiction: ", name,
@@ -457,19 +318,7 @@ server <- function(input, output) {
             filter(code == input$jurisdiction_select)
     })
     
-    title_text <- reactive({
-        date_string <- filteredStats()$date_of_full_vaccination %>%
-            with_tz(tzone = input$tz_select) %>%
-            sf()
-           
-       # text <- paste0(filteredStats()$name," will be fully vaccinated against COVID-19 on ", date_string, " ", tz_abbrs[[input$tz_select]], "!")
-        #message(text)
-        paste0(filteredStats()$name," will be fully vaccinated against COVID-19 on ", sf(filteredStats()$date_of_full_vaccination %>% with_tz(tzone = input$tz_select)), " ", tz_abbrs[[input$tz_select]], ".")
-        
-    })
-    
     output$title_text <- renderText({
-        date_string <- sf(filteredStats()$date_of_full_vaccination)
         paste0(filteredStats()$name," will be fully vaccinated against COVID-19 on ", sf(filteredStats()$date_of_full_vaccination %>% with_tz(tzone = input$tz_select)), " ", tz_abbrs[[input$tz_select]], "!")
     })
 }
