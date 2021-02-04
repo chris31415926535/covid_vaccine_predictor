bd1d3f173f5ea8322e1c3e8536b0ef628fc88e01
Christopher Belanger <christopher.a.belanger@gmail.com>
2021-02-03 20:51:41 -0500

    update og:description again

--- app.R
+++ app.R
@@ -190,7 +190,7 @@ ui <- dashboardPage(
         )),
         
         tags$head(HTML(
-            '<meta property="og:description" content="Good news! At the rate we\'re going, Canada and the provinces & territories will be fully vaccinated against COVID-19 by...">'
+            '<meta property="og:description" content="This dashboard calculates how long it will take to vaccinate all Canadians if things continue at their current rate. The conclusions may be humourous, but they\'re based on real data and the implications couldn\'t be more serious.">'
         )),
         
         
